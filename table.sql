4cdc876010673f608b8cf28b9fbeca8bb1bb20e8
Chirag Shah <cshah@phdata.io>
2024-10-01 12:05:18 +0530

    'added-sql-files'

---
+++ table.sql
@@ -0,0 +1,7 @@
+create table cicd.objects.test_table(
+    name varchar,
+    id int,
+    place varchar,
+    phone int
+);
+
---
+++ table_2.sql
@@ -0,0 +1,5 @@
+create table cicd.objects.test_table_2(
+    location varchar,
+    address varchar,
+    pin int
+);
